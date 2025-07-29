ec90edd574efdb4a5d80fdb647e122d49c722517
usefmahmud <usef.mahmoud@outlook.com>
2025-07-30 01:08:55 +0300

    refactor App component layout; pass streakDates and yearDates props to Calendar; add YEAR_DAYS mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Calendar from './components/calendar';
 import Streak from './components/streak';
+import { YEAR_DAYS } from './mock/date';
 
 function App() {
   return (
-    <div className='flex h-screen items-center justify-center gap-10 flex-col'>
+    <div className='flex h-screen flex-col items-center justify-center gap-10 p-6'>
       <Streak />
-      <Calendar />
+      <Calendar
+        streakDates={['2024-08-01', '2024-08-02']}
+        yearDates={YEAR_DAYS}
+      />
     </div>
   );
 }
